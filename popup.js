810173b3499b788e1ba8c11f080da0c0a8080648
19khowes <72945859+19khowes@users.noreply.github.com>
2021-07-28 12:06:04 -0600

    Styling Mostly

--- popup.js
+++ popup.js
@@ -24,7 +24,7 @@ displayInfoBtn.addEventListener("click", async () => {
 function getInfo() {
     // Get all iframes and put in info variable
     info = document.getElementsByTagName('iframe');
-    console.log(info);
+    // console.log(info);
     
     // Different regex variations for different iframe types
     const re1 = /entryid%2F(\w_\w{8})%2F/;
@@ -33,7 +33,7 @@ function getInfo() {
     // Loop through each iframe
     for (frame of info) {
         // regex match with each iframe's src
-        console.log(frame.src);
+        // console.log(frame.src);
         let match1 = frame.src.match(re1);
         let match2 = frame.src.match(re2);
 
@@ -45,11 +45,13 @@ function getInfo() {
             entryid = match2[1];
         }
         
-
-        let toAppend = document.createElement('p');
-        toAppend.style.color = "darkred";
+        // Create/Style the element (with id in it) to add on to the parent node of the iframe
+        let toAppend = document.createElement('span');
+        toAppend.style.color = "#1c746b";
         toAppend.style.fontSize = "24pt";
-        toAppend.innerHTML = entryid;
+        toAppend.style.backgroundColor = "#d6d6d6";
+        toAppend.innerHTML = `Entry/Playlist ID: ${entryid}`;
+        console.log(`Entry/Playlist ID: ${entryid}`)
         frame.parentNode.appendChild(toAppend);
         // https://cdnapisec.kaltura.com/p/1530551/embedPlaykitJs/uiconf_id/48353853?iframeembed=true&entry_id=1_t1thliam
     }
