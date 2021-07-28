480d4c7d84310a71b93e97fb14a86ce4c3acec74
19khowes <72945859+19khowes@users.noreply.github.com>
2021-07-28 11:41:47 -0600

    Two regex checkers of the iframe source for entry id and icon change

--- popup.js
+++ popup.js
@@ -22,14 +22,35 @@ displayInfoBtn.addEventListener("click", async () => {
 // The body of this function will be executed as a content script inside the
 // current page
 function getInfo() {
+    // Get all iframes and put in info variable
     info = document.getElementsByTagName('iframe');
     console.log(info);
+    
+    // Different regex variations for different iframe types
+    const re1 = /entryid%2F(\w_\w{8})%2F/;
+    const re2 = /entry_id=(\w_\w{8})/;
+    
+    // Loop through each iframe
     for (frame of info) {
+        // regex match with each iframe's src
+        console.log(frame.src);
+        let match1 = frame.src.match(re1);
+        let match2 = frame.src.match(re2);
+
+        // Default value of no match incase no regex match is found for any variation
+        let entryid = "no match";
+        if (match1) {
+            entryid = match1[1];
+        } else if (match2)  {
+            entryid = match2[1];
+        }
+        
+
         let toAppend = document.createElement('p');
         toAppend.style.color = "darkred";
         toAppend.style.fontSize = "24pt";
-        toAppend.innerHTML = "Entry Id Here";
+        toAppend.innerHTML = entryid;
         frame.parentNode.appendChild(toAppend);
-        console.log(frame.outerHTML);
+        // https://cdnapisec.kaltura.com/p/1530551/embedPlaykitJs/uiconf_id/48353853?iframeembed=true&entry_id=1_t1thliam
     }
 }
